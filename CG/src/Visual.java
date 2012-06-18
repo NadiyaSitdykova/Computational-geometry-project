8e8e724331ac4fe90dd88f9551334957a956d3c1
Nadiya Sitdykova <rovenskasa@gmail.com>
2012-06-18 20:48:52 +0400

    first commit

--- CG/src/Visual.java
+++ CG/src/Visual.java
@@ -5,11 +5,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Visual extends Canvas {
-	private int x1, x2, y, qx, qy1, qy2, qytmp;
+	private int x1, x2, y, qx, qy1, qy2;
 	private boolean clear = true;
 	private boolean first = true;
 	private boolean query = false;
-	private boolean ans = false;
 	private List<HorizontalInterval> L = new ArrayList<HorizontalInterval>();
 	private List<Point> points = new ArrayList<Point>();
 	private List<HorizontalInterval> answer = new ArrayList<HorizontalInterval>();
@@ -21,13 +20,10 @@ public class Visual extends Canvas {
 				if (!query) {
 					if (first) {
 						x1 = e.getX();
-						System.out.println("x1 " + x1);
 						y = e.getY();
-						System.out.println("y " + y);
 						first = false;
 					} else {
 						x2 = e.getX();
-						System.out.println("x2 " + x2);
 						HorizontalInterval interval = new HorizontalInterval(
 								Math.min(x1, x2), Math.max(x1, x2), y);
 						L.add(interval);
@@ -39,20 +35,15 @@ public class Visual extends Canvas {
 				} else {
 					if (first) {
 						qx = e.getX();
-						System.out.println("qx " + qx);
 						qy1 = e.getY();
-						System.out.println("qy1 " + qy1);
 						first = false;
 					} else {
 						qy2 = e.getY();
-						System.out.println("qy2 " + qy2);
 						first = true;
 						Collections.sort(points, new Xcomp());
-						System.out.println("searching");
 						IntervalTree InterTree = new IntervalTree(L, points);
 						answer.clear();
 						InterTree.queryIntervalTree(qx, Math.min(qy1, qy2), Math.max(qy1, qy2), answer);
-						System.out.println(answer.size());
 						repaint();
 
 					}
@@ -64,7 +55,6 @@ public class Visual extends Canvas {
 			public void keyTyped(KeyEvent e) {
 				if (e.getKeyChar() == '\n') {
 					query = true;
-					// repaint();
 				}
 			}
 		});
@@ -87,11 +77,9 @@ public class Visual extends Canvas {
 			g.setColor(Color.RED);
 			g.drawLine(qx, qy1, qx, qy2);
 			g.setColor(Color.GREEN);
-			System.out.println(answer.size());
 			for (int i = 0; i < answer.size(); i++) {
 				interval = answer.get(i);
 				g.drawLine(interval.x1, interval.y, interval.x2, interval.y);
-				System.out.println(interval.x1 + " " + interval.x2 + " " + interval.y);
 			}
 			clear = false;
 		}
